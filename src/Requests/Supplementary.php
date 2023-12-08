21deb5194a9f709fd687aef7715598cb0f83c400
edlugz <eddy.lugaye@gmail.com>
2023-12-08 12:26:58 +0300

    Update Supplementary.php
    add function to get channel from mobile number based on mobile network

--- src/Requests/Supplementary.php
+++ src/Requests/Supplementary.php
@@ -133,4 +133,27 @@ class Supplementary extends SasaPayClient
     {
         return $this->call($this->sasaPayAgentsEndPoint, [], 'GET');
     }
+
+    /**
+     * get channel code from mobile number
+     *
+	 * @param string $mobileNumber - 0XXXXXXXXX
+	 *
+     * @return string
+     */
+    public function getChannel(): string
+    {
+        $safaricom = '/(?:0)?((?:(?:7(?:(?:[01249][0-9])|(?:5[789])|(?:6[89])))|(?:1(?:[1][0-5])))[0-9]{6})$/';
+		$airtel = '/(?:0)?((?:(?:7(?:(?:3[0-9])|(?:5[0-6])|(8[5-9])))|(?:1(?:[0][0-2])))[0-9]{6})$/';
+		$telkom = '/(?:0)?(77[0-9][0-9]{6})/';
+		if(preg_match($safaricom, $str)){
+			return '63902';
+		}
+		if(preg_match($airtel, $str)){
+			return '63903';
+		}
+		if(preg_match($telkom, $str)){
+			return '63907';
+		}
+    }
 }
