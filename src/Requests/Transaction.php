6f2068cb4df17b8fe3d66148cffff0573ef7635b
edlugz <eddy.lugaye@gmail.com>
2023-12-10 12:40:50 +0300

    Update Transaction.php
    -include update transaction function

--- src/Requests/Transaction.php
+++ src/Requests/Transaction.php
@@ -33,18 +33,53 @@ class Transaction extends SasaPayClient
      */
     public function check($checkoutRequestId, $merchantTransactionReference, $transactionCode): mixed
     {
-        $parameters = [
-            'merchantCode'                 => $this->merchantCode,
-            'checkoutRequestId'            => $checkoutRequestId,
-            'merchantTransactionReference' => $merchantTransactionReference,
-            'transactionCode'              => $transactionCode,
-        ];
-
-        return $this->call($this->checkEndPoint, ['json' => $parameters]);
+		$payment = SasaPayTransaction::where('checkout_request_id', $checkoutRequestId)->first();
+		
+		if($payment){
+		
+			$parameters = [
+				'merchantCode'                 => $this->merchantCode,
+				'checkoutRequestId'            => $checkoutRequestId,
+				'merchantTransactionReference' => $merchantTransactionReference,
+				'transactionCode'              => $transactionCode,
+			];
+			
+			try {
+				$response = $this->call($this->checkEndPoint, ['json' => $parameters]);
+			} catch(SasaPayRequestException $e) {
+				$response = json_encode([
+					'status'         => false,
+					'responseCode'   => $e->getCode(),
+					'message'        => $e->getMessage(),
+				]);
+			}
+			
+			if ($response->status) {
+				$data = [
+					'result_code' => $response->data->ResultCode,
+					'result_desc' => $response->data->ResultDescription,
+					'merchant_transaction_reference',
+					'third_party_transaction_code' => $response->data->ThirdPartyTransactionCode,
+					'transaction_date' => $response->data->TransactionDate,
+					'destination_channel' => $response->data->DestinationChannel,
+					'source_channel' => $response->data->SourceChannel,
+					'sasapay_transaction_id' => $response->data->TransactionCode
+				];
+				
+				$payment->update($data);
+				
+				return response()->json(['status' => true, 'message' => 'Payment details updated successfully.']);
+			} else {
+				return response()->json(['status' => false, 'message' => 'Transaction details not found.']);
+			}
+		} else {
+			return response()->json(['status' => false, 'message' => 'Transaction does not exist.']);
+		}
+		
     }
 
     /**
-     * fetch your transactions statement directly from our API.
+     * verify transaction details directly from our API.
      *
      * @param $transactionCode
      *
