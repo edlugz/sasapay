3d0b9a42d92c9eb5d01ea5b4ac78db3631e6ec55
edlugz <eddy.lugaye@gmail.com>
2023-12-12 23:58:15 +0300

    update Sasapay models
    make all of attributes mass assignable

--- src/Models/SasaPayBeneficiary.php
+++ src/Models/SasaPayBeneficiary.php
@@ -11,41 +11,6 @@ class SasaPayBeneficiary extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = [
-        'account_id',
-        'first_name',
-        'middle_name',
-        'last_name',
-        'email',
-        'director_name',
-        'director_id_number',
-        'director_mobile_number',
-        'director_kra_pin',
-        'business_name',
-        'bill_number',
-        'mobile_number',
-        'country_code',
-        'sub_region',
-        'industry',
-        'sub_industry',
-        'bank_code',
-        'bank_account_number',
-        'business_type',
-        'registration_number',
-        'kra_pin',
-        'document_type',
-        'document_number',
-        'description',
-        'product_type',
-        'request_status',
-        'response_code',
-        'message',
-        'otp',
-        'request_id',
-        'confirmation_status',
-        'confirmation_message',
-        'confirmation_response_code',
-        'account_number',
-        'account_status',
-    ];
+    protected $guarded = [];
+	
 }
